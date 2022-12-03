fb6fd9e27582cee73661b009ee7831848a793cf4
Fakesum <worldanvilbild@gmail.com>
2022-12-03 17:33:28 +0530

    Added a find free port function

--- __init__.py
+++ __init__.py
@@ -82,3 +82,12 @@ def compare(a: str,b: str) -> float:
             if lchr == schr:
                 res += (1/len(max([a,b])))
     return res
+
+# find a free port
+def find_free_port():
+    import socket
+    from contextlib import closing
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
