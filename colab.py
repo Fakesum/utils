7fcc02d9892067a396896d36511e123c3a8f809c
Fakesum <worldanvilbild@gmail.com>
2022-12-03 19:34:03 +0530

    Intall now install python 3.10 on google colab

--- colab.py
+++ colab.py
@@ -1,5 +1,8 @@
 # Only for running on google colab
 def install():
-    !pip install undetected_chromedriver PyVirtualDisplay
-    !apt update
-    !apt install xvfb chromium-chromedriver
+    !apt-get update
+    !apt install chromium-chromedriver xvfb
+    !wget https://github.com/korakot/kora/releases/download/v0.10/py310.sh
+    !bash ./py310.sh -b -f -p /usr/local
+    !python -m ipykernel install --name "py310" --user
+    !python -m pip install undetected-chromedriver PyVirtualDisplay loguru
