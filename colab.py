ee815c5eab83116220c7f188246ccdd30fa54407
Fakesum <worldanvilbild@gmail.com>
2022-12-20 16:45:48 +0530

    Removed colab.py, Since Never Used it

--- colab.py
+++
@@ -1,8 +0,0 @@
-# Only for running on google colab
-def install():
-    !apt-get update
-    !apt install chromium-chromedriver xvfb
-    !wget https://github.com/korakot/kora/releases/download/v0.10/py310.sh
-    !bash ./py310.sh -b -f -p /usr/local
-    !python -m ipykernel install --name "py310" --user
-    !python -m pip install undetected-chromedriver PyVirtualDisplay loguru
