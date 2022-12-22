22a49cdda3d6aba6a655107a0bec06a33812fdda
Fakesum <worldanvilbild@gmail.com>
2022-12-22 19:56:03 +0530

    Temparary Image Assets Datatype with Generic Temp API

--- datatypes.py
+++ datatypes.py
@@ -18,3 +18,43 @@ class RoundTable(list):
                 return nlist
         else:
             return self[self.prev:num+self.prev]
+
+class __TempAssets(object):
+    import os as __os
+    import typing as __typing
+    def __init__(
+            self, 
+            contents: __typing.Any,
+            name: str,
+        ) -> None:
+        self.contents = contents
+        self.img = None
+        
+        self.name = name
+    
+    def __enter__(self):
+        self.write(self.contents)
+        return self
+    
+    def __exit__(self):
+        self.__os.remove(self.name)
+    
+    def write(self):
+        raise NotImplementedError
+    
+    def read(self):
+        raise NotImplementedError
+
+class TempImage(__TempAssets):
+    import cv2 as __cv2
+    import random as __random
+    def __init__(self, contents: __cv2.Mat, name: str = f"temp{__random.randint(10**3,10**4)}") -> None:
+        import pathlib
+        super().__init__(contents, "png", self.__os.path.join(pathlib.Path(__file__).parent.parent.resolve(),f"assets/temp/imgs/{name}.png"))
+    
+    def read(self):
+        return self.__cv2.imread(self.name)
+    
+    def write(self,contents):
+        self.contents = contents
+        self.__cv2.imwrite(self.name, contents)
